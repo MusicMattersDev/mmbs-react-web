532ad3fffe229a93f561f13796412bc0fdced04d
Jdot021 <127146096+Jdot021@users.noreply.github.com>
2023-10-19 13:11:54 -0500

    Modifying the login page and home page

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -267,7 +267,7 @@ function Calendar({ showSidebar, setShowSidebar }) {
                             >
                                 <strong style={{ fontSize: '16px'}}>{event.title}</strong>
                                 <br />
-                                <i style={{ fontSize: '12px' }}>{event.extendedProps.startTime} - {event.extendedProps.endTime}</i>
+                                <i style={{ fontSize: '13px' }}>{event.extendedProps.startTime} - {event.extendedProps.endTime}</i>
                             </div>
                         )}
                         contentHeight="auto"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,48 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import mmlogo from '../assets/icon-transparent.png'
 const Home = () => {
- 
-    return (
-        <div className='home'>
-          <center>
-            <h1>Welcome to Music Matters Booking System</h1>
-            <p>-R10.8.23</p>
-            <img src={ mmlogo } width="200" height="200"/>
-          </center>
-        </div>
-    )
+  return (
+      <div className='home' style={styles.container}>
+          <div style={styles.content}>
+              <img src={ mmlogo } alt="Music Matters Logo" style={styles.logo} />
+              <h1 style={styles.title}>Welcome to Music Matters Booking System</h1>
+              <p style={styles.subtitle}>-R10.8.23</p>
+          </div>
+      </div>
+  )
+}
+
+const styles = {
+  container: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "#f4f4f4",
+      minHeight: "100vh",
+      padding: "2em 0",
+  },
+  content: {
+      textAlign: 'center',
+      maxWidth: '90%',
+  },
+  logo: {
+      boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      width: "200px",
+      height: "200px",
+      maxWidth: '100%',
+      margin: '1em auto'
+  },
+  title: {
+      color: "#333",
+      marginTop: "1em",
+      fontSize: '1.5em',
+      wordWrap: 'break-word'
+  },
+  subtitle: {
+      color: "#777",
+      marginTop: "0.5em",
+      fontSize: '1em'
   }
-  
+}
+
 export default Home;
